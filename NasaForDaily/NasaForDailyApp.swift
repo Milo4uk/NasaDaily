d7f0d351dea95cccc4378a1066aa675633ed77c4
Milo4uk <wlir0tn@gmail.com>
2023-04-26 14:38:41 +0300

    Initial Commit

---
+++ NasaForDaily/NasaForDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NasaForDailyApp.swift
+//  NasaForDaily
+//
+//  Created by Mila ✨ on 26.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct NasaForDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
